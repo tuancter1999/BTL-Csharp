a0cd9949049ea601de9b9fa90a5949b265aec81c
Nguyễn Đình Tuân <51197663+tuancter1999@users.noreply.github.com>
2020-08-28 11:09:53 +0700

    .

---
+++ BUL/ChucVu_BUL.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUL
+{
+    public class ChucVu_BUL
+    {
+        private ChucVu_DAL chucVu_DAL = new ChucVu_DAL();
+        public DataTable getAllChucVu()
+        {
+            return chucVu_DAL.getAllChucVu();
+        }
+    }
+}
